19e5b888d8d4026c8c8ce2fbbde9c7a63ceeec51
Bisason Khanal <bisason10@gmail.com>
2024-05-02 23:26:18 -0400

    changing code to add two nos

--- addTwoNumbers/src/Main.java
+++ addTwoNumbers/src/Main.java
@@ -4,7 +4,11 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int a=10;
+        int b=20;
+        int sum=0;
+        sum = a+b;
+        System.out.println("sum=" +sum);
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
