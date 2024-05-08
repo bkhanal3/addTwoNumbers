314615bb030647aeecaf9c725457b1b88ada731c
Bisason Khanal <bisason10@gmail.com>
2024-05-07 22:57:34 -0400

    feature_khanal1012 chanes made in int value

--- addTwoNumbers/src/Main.java
+++ addTwoNumbers/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        int a=10;
+        int a=110;
         int b=20;
         int sum=0;
         sum = a+b;
